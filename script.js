08ab68d25ad8b47d117562549b8080dea69c73e0
Vishnuat18 <vishnurajan24766@gmail.com>
2025-02-21 05:56:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function controlDevice(command) {
-    fetch(`/${command}`)
-        .then(response => response.text())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
+let lightStates = [false, false, false];
+
+function toggleLight(lightNumber) {
+    const button = document.querySelector(`#light${lightNumber} button`);
+    const img = button.querySelector("img");
+    const span = button.querySelector("span");
+
+    lightStates[lightNumber - 1] = !lightStates[lightNumber - 1];
+
+    if (lightStates[lightNumber - 1]) {
+        img.src = "img/1739147870234.png"; // On state image
+        span.innerText = `Turn Off Light ${lightNumber}`;
+        fetch(`/light/${lightNumber}/on`);
+    } else {
+        img.src = "img/1739147870248.png"; // Off state image
+        span.innerText = `Turn On Light ${lightNumber}`;
+        fetch(`/light/${lightNumber}/off`);
+    }
 }
