52f7505c3df4e10e319f8a95257c14e93d4f79b4
Vishnuat18 <vishnurajan24766@gmail.com>
2025-02-22 08:12:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-let esp8266IP = "http://192.168.1.100"; // Replace with your ESP8266's IP
+let esp8266IP = "http://192.168.1.100"; // Replace with your ESP8266's actual IP
 
 function toggleLight(lightNumber) {
     const button = document.querySelector(`#light${lightNumber} button`);
     const img = button.querySelector("img");
     const span = button.querySelector("span");
 
+    // Ensure absolute image path comparison
     let isOn = img.src.includes("1739147870234.png"); // Check if the light is ON
     let action = isOn ? "off" : "on"; // Toggle action
 
     fetch(`${esp8266IP}/light/${lightNumber}/${action}`)
         .then(response => response.text())
         .then(data => {
-            console.log(data); // Debug response from ESP8266
-
+            console.log(data); // Debugging response from ESP8266
+            
+            // Ensure image change based on the response
             if (action === "on") {
-                img.src = "img/1739147870234.png"; // Change to ON image
+                img.src = window.location.origin + "/img/1739147870234.png"; // Light ON image
                 span.innerText = `Turn Off Light ${lightNumber}`;
             } else {
-                img.src = "img/1739147870248.png"; // Change to OFF image
+                img.src = window.location.origin + "/img/1739147870248.png"; // Light OFF image
                 span.innerText = `Turn On Light ${lightNumber}`;
             }
         })
-        .catch(error => console.error("Error:", error));
+        .catch(error => {
+            console.error("Error:", error);
+        });
 }
