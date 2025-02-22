34cf2ab38a7bb53bac0ff44dd2b722b116705a04
Vishnuat18 <vishnurajan24766@gmail.com>
2025-02-22 20:42:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,22 @@
-let esp8266IP = "http://192.168.114.160"; // Update with your ESP8266's IP
+let esp8266IP = "http://192.168.114.160"; // Your ESP8266 IP
 
 function toggleLight(lightNumber) {
     const button = document.querySelector(`#light${lightNumber} button`);
     const img = button.querySelector("img");
     const span = button.querySelector("span");
 
-    let isOn = img.src.includes("1739147870234.png"); // Check if the light is ON
-    let action = isOn ? "off" : "on"; // Toggle action
+    let isOn = img.src.includes("1739147870234.png"); // Check light state
+    let action = isOn ? "off" : "on";
 
-    fetch(`${esp8266IP}/light/${lightNumber}/${action}`, { method: "GET", mode: "no-cors" })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("ESP8266 is not responding!");
-            }
-            return response.text();
-        })
-        .then(data => {
-            console.log(data); // Debugging response from ESP8266
-
+    fetch(`${esp8266IP}/light/${lightNumber}/${action}`, { mode: "no-cors" })
+        .then(() => {
             if (action === "on") {
-                img.src = window.location.origin + "/img/1739147870234.png"; // Light ON image
+                img.src = "img/1739147870234.png";
                 span.innerText = `Turn Off Light ${lightNumber}`;
             } else {
-                img.src = window.location.origin + "/img/1739147870248.png"; // Light OFF image
+                img.src = "img/1739147870248.png";
                 span.innerText = `Turn On Light ${lightNumber}`;
             }
         })
-        .catch(error => {
-            console.error("Error:", error);
-            alert("ESP8266 is offline or unreachable. Please check the connection.");
-        });
+        .catch(error => console.error("Fetch Error:", error));
 }
