fedd663336c56d3b6922868bb3fb36836c5a4648
Vishnuat18 <vishnurajan24766@gmail.com>
2025-02-23 14:21:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-let esp8266IP = "http://192.168.114.160"; // Your ESP8266 IP
+// ESP8266 Static IP Address
+let esp8266IP = "http://192.168.114.160"; 
 
+// Function to toggle the light
 function toggleLight(lightNumber) {
     const button = document.querySelector(`#light${lightNumber} button`);
     const img = button.querySelector("img");
     const span = button.querySelector("span");
 
-    let isOn = img.src.includes("1739147870234.png"); // Check light state
-    let action = isOn ? "off" : "on";
+    let isOn = img.src.includes("1739147870234.png"); // Check if the light is ON
+    let action = isOn ? "off" : "on"; // Toggle between ON and OFF
 
-    fetch(`${esp8266IP}/light/${lightNumber}/${action}`, { mode: "no-cors" })
-        .then(() => {
+    // Send request to ESP8266
+    fetch(`${esp8266IP}/light/${lightNumber}/${action}`)
+        .then(response => response.text())
+        .then(data => {
+            console.log("ESP Response:", data); // Debugging response from ESP8266
+
             if (action === "on") {
-                img.src = "img/1739147870234.png";
+                img.src = "img/1739147870234.png"; // Change to ON image
                 span.innerText = `Turn Off Light ${lightNumber}`;
             } else {
-                img.src = "img/1739147870248.png";
+                img.src = "img/1739147870248.png"; // Change to OFF image
                 span.innerText = `Turn On Light ${lightNumber}`;
             }
         })
-        .catch(error => console.error("Fetch Error:", error));
+        .catch(error => {
+            console.error("Error:", error);
+            alert("ESP8266 is not reachable! Check the connection.");
+        });
 }
+
+// Ensure ESP is connected before clicking the button
+function checkESPConnection() {
+    fetch(`${esp8266IP}/`)
+        .then(response => {
+            if (response.ok) {
+                console.log("ESP8266 Connected!");
+            } else {
+                console.error("ESP8266 Connection Failed!");
+                alert("ESP8266 is not connected to the network.");
+            }
+        })
+        .catch(error => {
+            console.error("ESP8266 Not Found!", error);
+            alert("ESP8266 is not reachable! Check the IP address.");
+        });
+}
+
+// Call this function on page load to verify ESP connection
+window.onload = checkESPConnection;
