80559981239a29ab880482a0b2f7ee6739695323
Vishnuat18 <vishnurajan24766@gmail.com>
2025-02-23 10:58:22 -0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 function toggleLight(lightNumber) {
-  var xhr = new XMLHttpRequest();
-  var button = document.getElementById("light" + lightNumber);
-  button.querySelector("button");
-  var currentState = button.querySelector("span").innerText.includes("Off") ? "off" : "on";
-  var action = currentState === "off" ? "on" : "off";
+    var xhr = new XMLHttpRequest();
+    var button = document.getElementById("light" + lightNumber).querySelector("button");
+    var currentState = button.querySelector("span").innerText.includes("Off") ? "off" : "on";
+    var action = currentState === "off" ? "on" : "off";
 
-  xhr.open("GET", "https://192.168.114.160/light/" + lightNumber + "/" + action, true);
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      button.querySelector("span").innerText = "Turn " + (action === "on" ? "Off" : "On") + " Light " + lightNumber;
-    }
-  };
-  xhr.send();
+    xhr.open("GET", "http://192.168.114.160/light/" + lightNumber + "/" + action, true);
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            button.querySelector("span").innerText = "Turn " + (action === "on" ? "Off" : "On") + " Light " + lightNumber;
+        }
+    };
+    xhr.send();
 }
