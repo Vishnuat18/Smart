41627f3ac8dd4d82e0cc9db289e1c885cf129007
Vishnuat18 <vishnurajan24766@gmail.com>
2025-02-23 10:47:12 -0100

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function toggleLight(lightNumber) {
   var currentState = button.querySelector("span").innerText.includes("Off") ? "off" : "on";
   var action = currentState === "off" ? "on" : "off";
 
-  xhr.open("GET", "http://192.168.114.160/light/" + lightNumber + "/" + action, true);
+  xhr.open("GET", "https://192.168.114.160/light/" + lightNumber + "/" + action, true);
   xhr.onload = function() {
     if (xhr.status === 200) {
       button.querySelector("span").innerText = "Turn " + (action === "on" ? "Off" : "On") + " Light " + lightNumber;
