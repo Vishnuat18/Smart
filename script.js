439fe2c0dc93e4582bc85cfc53294437ccbe0101
Vishnuat18 <vishnurajan24766@gmail.com>
2025-02-23 16:51:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
 function toggleLight(lightNumber) {
   var xhr = new XMLHttpRequest();
-  var currentState = document.getElementById("light" + lightNumber).innerText;
-  var action = currentState.includes("Off") ? "on" : "off";
+  var action = "on";
 
   xhr.open("GET", "http://192.168.114.160/light/" + lightNumber + "/" + action, true);
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      var button = document.getElementById("light" + lightNumber).querySelector("button");
+      var currentState = button.querySelector("span").innerText.includes("Off") ? "Off" : "On";
+      if (currentState === "Off") {
+        action = "on";
+        button.querySelector("span").innerText = "Turn On Light " + lightNumber;
+      } else {
+        action = "off";
+        button.querySelector("span").innerText = "Turn Off Light " + lightNumber;
+      }
+      xhr.open("GET", "http://192.168.114.160/light/" + lightNumber + "/" + action, true);
+      xhr.send();
+    }
+  };
   xhr.send();
-
-  var button = document.getElementById("light" + lightNumber).querySelector("button");
-  if (action === "on") {
-    button.querySelector("span").innerText = "Turn Off Light " + lightNumber;
-  } else {
-    button.querySelector("span").innerText = "Turn On Light " + lightNumber;
-  }
 }
