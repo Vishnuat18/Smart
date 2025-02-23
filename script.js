9b89d3fad0c85f7da969048cdaf54eee13785f7e
Vishnuat18 <vishnurajan24766@gmail.com>
2025-02-23 11:26:00 -0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function toggleLight(lightNumber) {
     var currentState = button.querySelector("span").innerText.includes("Off") ? "off" : "on";
     var action = currentState === "off" ? "on" : "off";
 
-    xhr.open("GET", "http://192.168.114.160/light/" + lightNumber + "/" + action, true);
+    xhr.open("GET", `http://localhost:3000/light/${lightNumber}/${action}`, true); // Change URL to your proxy server's address
     xhr.onload = function() {
         if (xhr.status === 200) {
             button.querySelector("span").innerText = "Turn " + (action === "on" ? "Off" : "On") + " Light " + lightNumber;
